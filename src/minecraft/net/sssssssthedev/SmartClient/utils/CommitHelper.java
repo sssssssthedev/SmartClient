ebeb2a2977a591cfaa26d5f615b06322daa6bcf4
sssssssthedev <stefangreciuc40@gmail.com>
2021-06-28 21:32:17 +0300

    Update git to use my github working directory

--- src/minecraft/net/sssssssthedev/SmartClient/utils/CommitHelper.java
+++ src/minecraft/net/sssssssthedev/SmartClient/utils/CommitHelper.java
@@ -1,6 +1,7 @@
 package net.sssssssthedev.SmartClient.utils;
 
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
@@ -9,8 +10,10 @@ public class CommitHelper {
     public static CommitHelper instance = new CommitHelper();
 
     public String getCommitID() throws IOException {
-        Runtime rt = Runtime.getRuntime();
-        Process proc = rt.exec("git rev-parse HEAD");
+        ProcessBuilder builder = new ProcessBuilder( "git rev-parse HEAD");
+        builder.directory( new File("C:\\Users\\sssssss\\Downloads\\mcp918\\"));
+        builder.redirectErrorStream(true);
+        Process proc = builder.start();
         BufferedReader stdInput = new BufferedReader(new InputStreamReader(proc.getInputStream()));
         BufferedReader stdError = new BufferedReader(new InputStreamReader(proc.getErrorStream()));
         return stdInput.readLine().substring(0, 7);
