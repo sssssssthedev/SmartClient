2262ae6c9ef4d1fa3d7d6b3d89f41855e29cf198
sssssssthedev <stefangreciuc40@gmail.com>
2021-10-25 22:05:45 +0300

    Updated version

--- src/main/java/net/sssssssthedev/SmartClient/Main.java
+++ src/main/java/net/sssssssthedev/SmartClient/Main.java
@@ -33,7 +33,7 @@ public class Main {
 
     public static Main instance = new Main();
 
-    public static String build = "1.2.5";
+    public static String build = "1.2.6";
     public static String commit;
 
     static {
