19c329768111ed579c6170686066ee383278730a
sssssssthedev <stefangreciuc40@gmail.com>
2021-10-25 21:38:51 +0300

    Removed OsUtils and os switching in CommitHelper because it's useless

--- src/main/java/net/sssssssthedev/SmartClient/utils/CommitHelper.java
+++ src/main/java/net/sssssssthedev/SmartClient/utils/CommitHelper.java
@@ -8,23 +8,13 @@ public class CommitHelper {
 
     public static CommitHelper instance = new CommitHelper();
     public String getCommitID() throws IOException {
-        OsUtils.OSType osType = OsUtils.getOperatingSystemType();
         Properties properties = new Properties();
-        switch (osType) {
-            case Windows:
-                try (InputStream is = getClass().getResourceAsStream("/git.properties")) {
-                    properties.load(is);
-                    return properties.toString().replace("{", "").replace("}", "").replace("git.commit.id.abbrev=", "");
-                } catch (IOException io) {
-                    io.printStackTrace();
-                }
-            case Linux:
-                return "Linux";
-            case MacOS:
-                return "MacOS";
-            case Other:
-                return "Other";
+        try (InputStream is = getClass().getResourceAsStream("/git.properties")) {
+            properties.load(is);
+            return properties.toString().replace("{", "").replace("}", "").replace("git.commit.id.abbrev=", "");
+        } catch (IOException io) {
+            io.printStackTrace();
         }
-        return "";
+        return "null";
     }
 }
